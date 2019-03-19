c3c94998a0ec7e4029dee67f4509ce0722dd9251
lamyahamid <lamya.altuwairqi@gmail.com>
2019-03-19 22:36:05 +0300

    finish the home work

---
+++ main.js
@@ -0,0 +1,32 @@
+// function sayHello(name){
+//     console.log(`Hi, ${name}!`)
+// }
+
+const sayHello=(name)=>{console.log(`Hi, ${name}!`)}
+
+const getOlder=(age)=>{ console.log(age+50) }
+
+//____________________________________________________
+
+
+
+// const multiply = (num1, num2) => console.log(num1 * num2); 
+
+// const subtractFive = (num) => {
+//     const diff = 5 - num;
+// 	console.log(diff);
+// }
+
+// const printName = name = function() {
+//     console.log(name)
+// }
+
+
+
+//_____________________________________________________
+
+module.exports = {
+    hi: sayHello,
+    older: getOlder
+}
+
---
+++ main2.js
@@ -0,0 +1,7 @@
+const person = require('./main')
+
+
+console.log(person.hi("Anoud"));
+
+console.log(person.getOlder(12));
+
